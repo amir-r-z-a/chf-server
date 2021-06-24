b7f644d67197f03247b4f65570467ab58731d03b
arman heidari <armanartgallery@gmail.com>
2021-06-24 13:34:57 +0430

    profileScreen was finished

--- src/RestaurantAccount.java
+++ src/RestaurantAccount.java
@@ -25,17 +25,39 @@ public class RestaurantAccount {
     String signUp() {
         DataBase.getSingleTone().getController("RestaurantAccounts").writeFile(data.get("phoneNumber")
                 + ": {, " + data.get("name") + ", " + data.get("password") + ", "
-                + data.get("open") + ", " + data.get("close") + ", " + data.get("restaurantType") + ", null, " + "}\n");
+                + data.get("open") + ", " + data.get("close") + ", " + data.get("restaurantType")
+                + ", null, null, null, null, " + "}\n");
         return "valid";
     }
 
     String signIn() {
         String alreadyPhoneNumber = alreadyPhoneNumber();
-        if (alreadyPhoneNumber.equals("invalid")
-                || !alreadyPhoneNumber.split(", ")[2].equals(data.get("password"))) {
+        if (alreadyPhoneNumber.equals("invalid")) {
             return "invalid";
+        } else if (!alreadyPhoneNumber.split(", ")[2].equals(data.get("password"))) {
+            return "invalid-match";
         }
         return "valid";
+    }
+
+    String getName() {
+        return alreadyPhoneNumber().equals("invalid") ? "invalid" : alreadyPhoneNumber().split(", ")[1];
+    }
+
+    String getEmail() {
+        return alreadyPhoneNumber().equals("invalid") ? "invalid" : alreadyPhoneNumber().split(", ")[6];
+    }
+
+    String getAddress() {
+        return alreadyPhoneNumber().equals("invalid") ? "invalid" : alreadyPhoneNumber().split(", ")[7];
+    }
+
+    String getLatLang() {
+        return alreadyPhoneNumber().equals("invalid") ? "invalid" : alreadyPhoneNumber().split(", ")[8];
+    }
+
+    String getRadius() {
+        return alreadyPhoneNumber().equals("invalid") ? "invalid" : alreadyPhoneNumber().split(", ")[9];
     }
 
     String editName() {
@@ -52,6 +74,28 @@ public class RestaurantAccount {
                     strBuilder.append(update[i]).append(", ");
                 }
                 strBuilder.append(update[n - 1]);
+                str = strBuilder.toString();
+            }
+            ans.append(str).append("\n");
+        }
+        DataBase.getSingleTone().getController("RestaurantAccounts").writeFile(ans.toString(), true);
+        return "valid";
+    }
+
+    String editPhoneNumber() {
+        String phoneNumber = data.get("phoneNumber");
+        data.put("phoneNumber", data.get("newPhoneNumber"));
+        if (!alreadyPhoneNumber().equals("invalid")) {
+            return "invalid";
+        }
+        data.put("phoneNumber", phoneNumber);
+        String users = DataBase.getSingleTone().getController("RestaurantAccounts").readFile();
+        String[] split = users.split("\n");
+        StringBuilder ans = new StringBuilder();
+        for (String str : split) {
+            if (str.startsWith(data.get("phoneNumber"))) {
+                StringBuilder strBuilder = new StringBuilder(str);
+                strBuilder.replace(0, str.indexOf(":"), data.get("newPhoneNumber"));
                 str = strBuilder.toString();
             }
             ans.append(str).append("\n");
@@ -83,16 +127,85 @@ public class RestaurantAccount {
     }
 
     String editPassword() {
-        if (!alreadyPhoneNumber().split(", ")[2].equals(data.get("oldPassword"))) {
-            return "invalid";
+        String returnValue = "";
+        returnValue += !alreadyPhoneNumber().split(", ")[2].equals(data.get("oldPassword")) ?
+                "invalid-" : "valid-";
+        returnValue += !data.get("newPassword").equals(data.get("confirmPassword")) ? "invalid" : "valid";
+        if (returnValue.equals("valid-valid")) {
+            String users = DataBase.getSingleTone().getController("RestaurantAccounts").readFile();
+            String[] split = users.split("\n");
+            StringBuilder ans = new StringBuilder();
+            for (String str : split) {
+                if (str.startsWith(data.get("phoneNumber"))) {
+                    String[] update = str.split(", ");
+                    update[2] = data.get("newPassword");
+                    StringBuilder strBuilder = new StringBuilder(update[0] + ", ");
+                    int n = update.length;
+                    for (int i = 1; i < n - 1; i++) {
+                        strBuilder.append(update[i]).append(", ");
+                    }
+                    strBuilder.append(update[n - 1]);
+                    str = strBuilder.toString();
+                }
+                ans.append(str).append("\n");
+            }
+            DataBase.getSingleTone().getController("RestaurantAccounts").writeFile(ans.toString(), true);
         }
+        return returnValue;
+    }
+
+    String editAddress() {
         String users = DataBase.getSingleTone().getController("RestaurantAccounts").readFile();
         String[] split = users.split("\n");
         StringBuilder ans = new StringBuilder();
         for (String str : split) {
             if (str.startsWith(data.get("phoneNumber"))) {
                 String[] update = str.split(", ");
-                update[2] = data.get("newPassword");
+                update[7] = data.get("address");
+                StringBuilder strBuilder = new StringBuilder(update[0] + ", ");
+                int n = update.length;
+                for (int i = 1; i < n - 1; i++) {
+                    strBuilder.append(update[i]).append(", ");
+                }
+                strBuilder.append(update[n - 1]);
+                str = strBuilder.toString();
+            }
+            ans.append(str).append("\n");
+        }
+        DataBase.getSingleTone().getController("RestaurantAccounts").writeFile(ans.toString(), true);
+        return "valid";
+    }
+
+    String editLatLang() {
+        String users = DataBase.getSingleTone().getController("RestaurantAccounts").readFile();
+        String[] split = users.split("\n");
+        StringBuilder ans = new StringBuilder();
+        for (String str : split) {
+            if (str.startsWith(data.get("phoneNumber"))) {
+                String[] update = str.split(", ");
+                update[8] = data.get("latLang");
+                StringBuilder strBuilder = new StringBuilder(update[0] + ", ");
+                int n = update.length;
+                for (int i = 1; i < n - 1; i++) {
+                    strBuilder.append(update[i]).append(", ");
+                }
+                strBuilder.append(update[n - 1]);
+                str = strBuilder.toString();
+            }
+            ans.append(str).append("\n");
+        }
+        DataBase.getSingleTone().getController("RestaurantAccounts").writeFile(ans.toString(), true);
+        return "valid";
+    }
+
+    String editRadius() {
+        String users = DataBase.getSingleTone().getController("RestaurantAccounts").readFile();
+        String[] split = users.split("\n");
+        StringBuilder ans = new StringBuilder();
+        for (String str : split) {
+            if (str.startsWith(data.get("phoneNumber"))) {
+                String[] update = str.split(", ");
+                update[9] = data.get("radius");
                 StringBuilder strBuilder = new StringBuilder(update[0] + ", ");
                 int n = update.length;
                 for (int i = 1; i < n - 1; i++) {
--- src/Server.java
+++ src/Server.java
@@ -11,7 +11,8 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(2442);
             DataBase.getSingleTone().addDataBase("RestaurantAccounts", new Controller("D:\\Code\\DataBase\\Restaurant\\RestaurantAccounts.txt"));
-//            DataBase.getSingleTone().addDataBase("MenuEdition", new Controller("D://Code//DataBase//Restaurant//MenuEdition.txt"));
+            DataBase.getSingleTone().addDataBase("RestaurantCategory", new Controller("D:\\Code\\DataBase\\Restaurant\\RestaurantCategory.txt"));
+            DataBase.getSingleTone().addDataBase("RestaurantMenuEdition", new Controller("D:\\Code\\DataBase\\Restaurant\\RestaurantMenuEdition.txt"));
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Connected");
@@ -83,60 +84,139 @@ class RequestHandler extends Thread {
     @Override
     public void run() {
 //        IOExeption-->Exeption
+//        editName,.... --> edit
+//        getName,....--> get
         System.out.println("ready");
         String command = listener();
         System.out.println("command is: " + command);
         String[] split = command.split("-");
         if (split[0].equals("Restaurant")) {
+            HashMap<String, String> data;
+            RestaurantAccount restaurantAccount;
+            StringBuilder finalWrite = new StringBuilder();
             switch (split[1]) {
                 case "RestaurantSignUp": {
                     if (split[2].equals("alreadyPhoneNumber")) {
-                        HashMap<String, String> data = new HashMap<>(
+                        data = new HashMap<>(
                                 Map.of("phoneNumber", split[3]));
-                        RestaurantAccount restaurantAccount = new RestaurantAccount(data);
+                        restaurantAccount = new RestaurantAccount(data);
                         writer(restaurantAccount.alreadyPhoneNumber());
                     } else {
-                        HashMap<String, String> data = new HashMap<>(
+                        data = new HashMap<>(
                                 Map.of("name", split[2], "phoneNumber", split[3], "password", split[4],
                                         "open", split[5], "close", split[6], "restaurantType", split[7],
-                                        "email", "null"));
-                        RestaurantAccount restaurantAccount = new RestaurantAccount(data);
+                                        "email", "null", "address", "null", "latLang", "null", "radius", "null"));
+                        restaurantAccount = new RestaurantAccount(data);
                         writer(restaurantAccount.signUp());
                     }
                     break;
                 }
                 case "RestaurantSignIn": {
-                    HashMap<String, String> data = new HashMap<>(
+                    data = new HashMap<>(
                             Map.of("phoneNumber", split[2], "password", split[3]));
-                    RestaurantAccount restaurantAccount = new RestaurantAccount(data);
+                    restaurantAccount = new RestaurantAccount(data);
                     writer(restaurantAccount.signIn());
                     break;
                 }
-                case "EditProfile":
-                    HashMap<String, String> data;
-                    RestaurantAccount restaurantAccount;
+                case "RestaurantGetData":
                     switch (split[2]) {
                         case "name":
                             data = new HashMap<>(
-                                    Map.of("phoneNumber", split[3], "newName", split[4]));
+                                    Map.of("phoneNumber", split[3]));
                             restaurantAccount = new RestaurantAccount(data);
-                            writer(restaurantAccount.editName());
+                            writer(restaurantAccount.getName());
+                            break;
+                        case "email":
+                            data = new HashMap<>(
+                                    Map.of("phoneNumber", split[3]));
+                            restaurantAccount = new RestaurantAccount(data);
+                            writer(restaurantAccount.getEmail());
+                            break;
+                        case "address":
+                            data = new HashMap<>(
+                                    Map.of("phoneNumber", split[3]));
+                            restaurantAccount = new RestaurantAccount(data);
+                            writer(restaurantAccount.getAddress());
+                            break;
+                        case "radius":
+                            data = new HashMap<>(
+                                    Map.of("phoneNumber", split[3]));
+                            restaurantAccount = new RestaurantAccount(data);
+                            writer(restaurantAccount.getRadius());
+                            break;
+                    }
+                    break;
+                case "RestaurantEditProfile":
+                    switch (split[2]) {
+                        case "feature":
+                            finalWrite = new StringBuilder();
+                            for (int i = 3; i < split.length; i += 3) {
+                                if (i != 3) {
+                                    finalWrite.append("-");
+                                }
+                                switch (split[i]) {
+                                    case "name":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1], "newName", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editName());
+                                        break;
+                                    case "phoneNumber":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1], "newPhoneNumber", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editPhoneNumber());
+                                        break;
+                                    case "email":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1], "newEmail", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editEmail());
+                                        break;
+                                }
+                            }
+                            writer(finalWrite.toString());
                             break;
                         case "password":
                             data = new HashMap<>(
                                     Map.of("phoneNumber", split[3],
-                                            "oldPassword", split[4], "newPassword", split[5]));
+                                            "oldPassword", split[4], "newPassword", split[5], "confirmPassword", split[6]));
                             restaurantAccount = new RestaurantAccount(data);
                             writer(restaurantAccount.editPassword());
                             break;
-                        case "email":
-                            data = new HashMap<>(
-                                    Map.of("phoneNumber", split[3], "newEmail", split[4]));
-                            restaurantAccount = new RestaurantAccount(data);
-                            writer(restaurantAccount.editEmail());
+                        case "location":
+                            finalWrite = new StringBuilder();
+                            for (int i = 3; i < split.length; i += 3) {
+                                if (i != 3) {
+                                    finalWrite.append("-");
+                                }
+                                switch (split[i]) {
+                                    case "address":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1],
+                                                        "address", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editAddress());
+                                        break;
+                                    case "latLang":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1],
+                                                        "latLang", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editLatLang());
+                                        break;
+                                    case "radius":
+                                        data = new HashMap<>(
+                                                Map.of("phoneNumber", split[i + 1],
+                                                        "radius", split[i + 2]));
+                                        restaurantAccount = new RestaurantAccount(data);
+                                        finalWrite.append(restaurantAccount.editRadius());
+                                        break;
+                                }
+                            }
+                            writer(finalWrite.toString());
                             break;
                     }
-                    break;
             }
         } else if (command.equals("Client")) {
 
